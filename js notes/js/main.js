340e49222c8078eb13ce017ebe3ec1b87294e7d4
Tohier <tohierdreyer17@gmail,com>
2019-02-12 11:09:52 +0200

    notes

---
+++ js notes/js/main.js
@@ -0,0 +1,43 @@
+// NOTES
+
+//'use strict'
+// the "use strict" directive switches the engine to the "modern" mode, changing the behavior of some built-in features
+// prompt can be used to make popups 
+// alert is also used to make popups
+// the + operator can be used to join two strings together, otherwise called concatenation
+
+// ******************************************************************************
+
+// choose meaningful names
+// Don't use reserved words
+// Follow conventions
+
+// * https: *
+// develper.mozilla.org/en-US/docs/web/JavaScript/Reference/
+
+
+// ******************************************************************************
+
+console.log("Hello JavaScript");
+// this is how u make a variable
+
+var age =19;
+    name= " Toyer";
+    gender= " male"
+
+console.log(name);
+console.log(age);
+console.log(gender);
+
+// pop up message 
+
+let message = "Yo mybluw";
+alert(message + name);
+
+var cake = " Sponge Cake";
+    layers = 6;
+    flavour = " Chocolate";
+    price = " R2000";
+
+// prompt(message); 
+console.log(message + name + " your " + flavour + cake + " with " + layers + " layers" + " is ready for valentine's day." + " It is " + price + " for the cake. ");
